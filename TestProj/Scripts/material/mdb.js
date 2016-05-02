3171b36e552519eb61f69c2d8e0a3e23f729f26a
ze333 <korzun-aleksandr@yandex.ru>
2016-05-02 19:03:17 +0300

    Fix bug
    *fix with input type datetime

--- TestProj/Scripts/material/mdb.js
+++ TestProj/Scripts/material/mdb.js
@@ -4966,7 +4966,7 @@ if ($) {
 
         // Function to update labels of text fields
         Materialize.updateTextFields = function () {
-            var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
+            var input_selector = 'input[type=text],  input[type=datetime], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
             $(input_selector).each(function (index, element) {
                 if ($(element).val().length > 0 || $(this).attr('placeholder') !== undefined || $(element)[0].validity.badInput === true) {
                     $(this).siblings('label, i').addClass('active');
@@ -4977,7 +4977,7 @@ if ($) {
         };
 
         // Text based inputs
-        var input_selector = 'input[type=text], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
+        var input_selector = 'input[type=text], input[type=datetime], input[type=password], input[type=email], input[type=url], input[type=tel], input[type=number], input[type=search], textarea';
 
         // Handle HTML5 autofocus
         $('input[autofocus]').siblings('label, i').addClass('active');
